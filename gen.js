a81eb1c43c6c654394fb54026b4647fffc03e55f
unknown <133164451+Oduluyidavid@users.noreply.github.com>
2024-10-15 00:25:34 +0100

    first commit

--- gen.js
+++ gen.js
@@ -1,4 +1,7 @@
-// Class in javaScript simply denote a template for javaScript object
+// Class in javaScript can be defined as a template for javaScript object
+
+// Constructor is used to set up an actual object, A constructor gets called when an object 
+// is created using the new keyword, it is also called when an object is created using the new keyword.
 
 class Car{
     constructor(name, year){
@@ -9,7 +12,39 @@ class Car{
 const myCar1 = new Car("Camry" , 2008);
 const myCar2 = new Car("Audi" , 2007);
 
-console.log(myCar1,myCar2)
-// Car.map((year,name)=>{
-    // console.log(name, year)
-// })
+console.log("Camry" ,2008, "Audi", 2007)
+
+
+// class -> Object
+
+// instance properties: what they have
+// -name
+// -age
+//  -height
+
+//  instance method: what they do
+//  -talk
+//  -run
+//  - jump
+ 
+//  instance Properties
+class Rectangle{
+    // is used to setup
+    constructor( _width , _height, _color){
+        console.log("the Rectangle is being created!")
+        this.width = _width
+        this.height = _height
+        this.color= _color
+    }  
+    //  The instance Method
+    getArea(){
+        return this.width * this.height;
+    }
+    printDescription(){
+        console.log(`I am a Rectangle ${this.width} x ${this.height} and I am ${this.color}`);
+    } 
+}
+let myRectangle1 = new Rectangle( 5, 3, "blue");
+let myRectangle2 = new Rectangle( 10, 5, "red");
+
+myRectangle1.printDescription();
